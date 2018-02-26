98d5811783b5c26f21523941172670ea4e88eb20
saadou11 <saadallah.mahi@gmail.com>
2018-02-26 22:44:11 +0100

    Backend nouvelle version

---
+++ L2J1/src/main/java/main/Application.java
@@ -0,0 +1,13 @@
+package main;
+
+import org.springframework.boot.SpringApplication;
+
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+
+
+	}
+
+}
