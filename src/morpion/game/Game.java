e8b367c81cea34acaf43044902c02e8af4c70937
dekkar <ahmed_15@live.com>
2018-02-26 14:56:20 +0100

    ttt

---
+++ src/morpion/game/Game.java
@@ -0,0 +1,90 @@
+package morpion.game;
+
+import java.util.Scanner;
+
+import moprion.Joueur.Joueur;
+import morpion.grille.Grille;
+import saisie.Input;
+
+public class Game {
+	/// tous les champs 
+	 private Grille grille;
+	 private Joueur joueur1;
+	 private Joueur joueur2;
+	 private Joueur currentJoueur;
+	 
+	///// constructeur 
+	 public Game () {
+		 this.grille = new Grille();
+		 this.joueur1 = new Joueur(0, 'O');// premier joueur avec le O
+		 this.joueur2 = new Joueur(1, 'X');// deuxieme joueur avec le x 
+		 
+	 }
+	//// methodes 
+	 
+	 // methode priver 
+	 
+	 //// tour a tour pour jouer 
+
+private void swapTurn() {
+	if (this.currentJoueur.equals(this.joueur1))
+		this.currentJoueur = joueur2;
+	else
+		this.currentJoueur = joueur1;
+	
+}
+// controler l entre des donnee
+ private int getUserInput (Scanner scan ) {
+	int  input = -1;
+	 while (true) {
+		 System.out.println("c a votre tour de jouer  joueur "+this.currentJoueur.getCaractere());
+	     input = Input.readInt(scan);
+	   if ( (0 <= input) || (input <= 8) )
+		 break;
+	 }
+	return input;
+ }
+ private void afficherGrille() {
+	 System.out.println(this.grille);
+	 
+ }
+	 
+	// lancer le jeu
+	public void start() {
+		Scanner scan = new Scanner(System.in);
+		 currentJoueur = this.joueur1;
+		 while(true) {
+			 
+			 ///affficher la frille 
+			 afficherGrille();
+			 System.out.flush();
+			 // pour modifier la grille 
+			/// System.out.println(this.grille.toString());
+			 // pour lire un charactere 
+			 
+			 int input = getUserInput(scan);
+			 this.grille.setCaseContenu( input, this.currentJoueur.getCaractere());
+			  if(this.grille.getGagnant() != null) {
+				  afficherGrille();
+				  System.out.println("le joueur "+currentJoueur.getCaractere()+ " a gagner bravo !!");
+				  break;
+				// a current player won
+			  }else if (this.grille.estPlein()) {
+				  afficherGrille();
+				  System.out.println(" Drawwwww ");
+				  break;
+		 }
+				  //match null
+				  
+		   swapTurn(); 
+		 }
+		 scan.close();
+		
+	}
+	 ///refaire le jeu
+	// il ne faut une methode resete dans la  classe grille 
+	public void reset () {
+		this.grille.clear();
+		
+	}
+}
---
+++ src/morpion/grille/CaseGrille.java
@@ -0,0 +1,43 @@
+package morpion.grille;
+import moprion.Joueur.Joueur;
+
+public class CaseGrille {
+	//////// tous le champs 
+	
+	  ///// le id de la case 
+		private int  caseId ;
+		/////// le contenu de la case X ou O 
+		private Jetons contenu;
+
+	/////// contructeur 
+	public CaseGrille(int caseId) {
+		this.caseId = caseId;
+		this.contenu = null; // car y a pas de jetons a mettre des le debut 
+		
+	}
+	public CaseGrille(int caseId , Jetons contenu) {
+		this.caseId = caseId;
+		this.contenu = contenu;
+	}
+	///Getter // Setter 
+	public Joueur getPrioritairejetons() {
+		return get.getProprietaire();
+	}
+	
+	
+	public void setContenu(Jetons contenu) {
+		this.contenu = contenu;
+	}
+	////// Methodes
+	/// verifier si la case est rempler ou pas 
+	public boolean estVide() {
+		return this.contenu==null;
+	}
+	@Override
+	public String toString() {
+		if (this.contenu != null)
+		return this.contenu.toString() ;
+		else 
+		 return Integer.toString(caseId);
+	}
+}
