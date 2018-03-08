f459f1846318d484af4992bca55abcf332a06ae8
Mickael Dai <mickael.dai2609@gmail.com>
2018-03-08 12:56:24 +0100

    Nouvelle branche pour le site...

---
+++ siteL2J1/src/app/contacter/contacter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contacter',
+  templateUrl: './contacter.component.html',
+  styleUrls: ['./contacter.component.css']
+})
+export class ContacterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
