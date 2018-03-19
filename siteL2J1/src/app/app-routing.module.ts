07d5f9b377d3407613d89fbf1f0acad71a8f39d9
Tarujan <tarujan.vallipuram@gmail.com>
2018-03-19 17:58:10 +0100

    Routing du morpion

--- siteL2J1/src/app/app-routing.module.ts
+++ siteL2J1/src/app/app-routing.module.ts
@@ -12,6 +12,8 @@ import { SudokuNormalComponent }  from './sudoku-normal/sudoku-normal.component'
 import { SudokuDifficileComponent }  from './sudoku-difficile/sudoku-difficile.component';
 import { SudokuIndexComponent } from './sudoku-index/sudoku-index.component';
 import { MorpionIndexComponent }     from './morpion-index/morpion-index.component';
+import { MorpionJ1vsIaComponent } from './morpion-j1vs-ia/morpion-j1vs-ia.component';
+import { MorpionJ1vsJ2Component } from './morpion-j1vs-j2/morpion-j1vs-j2.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/index', pathMatch: 'full' },
@@ -25,7 +27,10 @@ const routes: Routes = [
   { path: 'sudoku-normal', component: SudokuNormalComponent},
   { path: 'sudoku-difficile', component: SudokuDifficileComponent},
   { path: 'sudoku-index', component: SudokuIndexComponent},
-  { path: 'morpion-index', component: MorpionIndexComponent }
+  { path: 'morpion-index', component: MorpionIndexComponent },
+  { path: 'morpion-j1vs-ia', component: MorpionJ1vsIaComponent },
+  { path: 'morpion-j1vs-j2', component: MorpionJ1vsJ2Component }
+
 ];
 
 @NgModule({
