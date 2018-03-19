111bfe3d64792021b8c680ec9b9aa008e3095e25
Mickael Dai <mickael.dai2609@gmail.com>
2018-03-19 14:03:20 +0100

    création du component sudoku-index

---
+++ siteL2J1/src/app/sudoku-index/sudoku-index.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sudoku-index',
+  templateUrl: './sudoku-index.component.html',
+  styleUrls: ['./sudoku-index.component.css']
+})
+export class SudokuIndexComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
