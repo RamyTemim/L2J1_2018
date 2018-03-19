7ec90afa405f8bd3013ae0a47ce497dccae7bec8
saadou11 <saadallah.mahi@gmail.com>
2018-03-19 12:30:54 +0100

    lien front et sudokuController

--- L2J1/src/main/java/main/Application.java
+++ L2J1/src/main/java/main/Application.java
@@ -23,6 +23,7 @@ public class Application {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/*").allowedOrigins("http://localhost:4200");
+				registry.addMapping("/sudoku/*").allowedOrigins("http://localhost:4200");
 			}
 		};
 	}
