fd5f3e28dce7353c050d6cfa74eedcfa8f0106a7
Tarujan <tarujan.vallipuram@gmail.com>
2018-03-19 15:27:38 +0100

    Ajout du component J1 vs J2

---
+++ siteL2J1/src/app/morpion-j1vs-j2/morpion-j1vs-j2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-morpion-j1vs-j2',
+  templateUrl: './morpion-j1vs-j2.component.html',
+  styleUrls: ['./morpion-j1vs-j2.component.css']
+})
+export class MorpionJ1vsJ2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
