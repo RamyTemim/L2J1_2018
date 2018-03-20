2a5ba6b03eeb8bbfa2d922275de8357a6a38a1cd
Tarujan <tarujan.vallipuram@gmail.com>
2018-03-20 16:16:24 +0100

    Ajout d'un component joueur

---
+++ siteL2J1/src/app/joueur/joueur.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-joueur',
+  templateUrl: './joueur.component.html',
+  styleUrls: ['./joueur.component.css']
+})
+export class JoueurComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
