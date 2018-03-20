300d66649996c8e2eaf8ee75f7a295936122622a
Tarujan <tarujan.vallipuram@gmail.com>
2018-03-20 19:31:20 +0100

    Ajout de joueur.ts contenant la valeur par défaut que l'on peut changer (il y n'y a pas de nom pour l'instant)

--- siteL2J1/src/app/joueur/joueur.component.ts
+++ siteL2J1/src/app/joueur/joueur.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Joueur } from '../joueur';
 
 @Component({
   selector: 'app-joueur',
@@ -7,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JoueurComponent implements OnInit {
 
+  joueur: Joueur ={
+    nom: ''
+  }
   constructor() { }
 
   ngOnInit() {
---
+++ siteL2J1/src/app/joueur/joueur.ts
@@ -0,0 +1,3 @@
+export class Joueur {
+  nom : string;
+}
