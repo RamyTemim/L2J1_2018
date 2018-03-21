88fa9a7cb2e1f0a72e317e541e8593163328b924
dekkar <ahmed_15@live.com>
2018-03-21 03:38:00 +0100

    tjr avec les class detail morpion

---
+++ L2J1/src/main/java/morpion/GameStatus.java
@@ -0,0 +1,10 @@
+package Enumeration;
+
+public enum GameStatus {
+IN_PROGRESS,
+FISRT_PLAYER_WON,
+SECOND_PLAYER_WON,
+DRAW,
+TIE,
+TIMEOUT
+}
---
+++ L2J1/src/main/java/morpion/GameType.java
@@ -0,0 +1,6 @@
+package Enumeration;
+
+public enum GameType {
+COMPUTER,
+COMPETITION
+}
