bc0f11c300b988c275019f322b4115aa92428f7f
saadou11 <saadallah.mahi@gmail.com>
2018-03-24 23:50:28 +0100

    delete zero number

--- siteL2J1/src/app/sudoku-difficile/sudoku-difficile.component.ts
+++ siteL2J1/src/app/sudoku-difficile/sudoku-difficile.component.ts
@@ -9,33 +9,21 @@ import 'rxjs/add/operator/map';
 })
 export class SudokuDifficileComponent implements OnInit {
   private sudokuDifficileUrl = 'http://localhost:8080/sudoku/sudokuDifficile';
-  private solution = 'http://localhost:8080/sudoku/solvedSudoku';
   data3: number[][];
-  data3sol: any ={};
 
 
   constructor(private http: Http) {
   this.getGrilleDifficile();
-   this.getGrilleSolved();
   }
 
   getDataDifficile(){
     return this.http.get(this.sudokuDifficileUrl)
     .map((res: Response) => res.json())
   }
-     getDataSolution(){
-    return this.http.get(this.solution)
-    .map((res: Response) => res.json())
-  }
   
   getGrilleDifficile(){
     this.getDataDifficile().subscribe(data3 =>{
       this.data3 = data3
-    })
-  }
-       getGrilleSolved(){
-    this.getDataSolution().subscribe(data3sol =>{
-      this.data3sol = data3sol
     })
   }
 
