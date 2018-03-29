1f47c82a4d7613544ad9cbf7271216cc1cf03c3e
saadou11 <saadallah.mahi@gmail.com>
2018-03-29 12:35:53 +0200

    ajout de post

--- siteL2J1/src/app/sudoku-facile/sudoku-facile.component.ts
+++ siteL2J1/src/app/sudoku-facile/sudoku-facile.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import 'rxjs/add/operator/map';
+
 
 @Component({
   selector: 'app-sudoku-facile',
@@ -12,8 +14,10 @@ export class SudokuFacileComponent implements OnInit {
   private sudokuFacileUrl = 'http://localhost:8080/sudoku/sudokuFacile';
   data1:  number[][];
 
-  constructor(private http: Http) {
+  constructor(private http: Http,private http2: HttpClient) {
+
   this.getGrilleFacile();
+  
   }
 
   getDataFacile(){
@@ -22,14 +26,30 @@ export class SudokuFacileComponent implements OnInit {
   }
   
   getGrilleFacile(){
-    this.getDataFacile().subscribe(data1 =>{
+	this.getDataFacile().subscribe(data1 =>{
       this.data1 = data1
     })
   }
   
-
   ngOnInit() {
+
   }
+  
+  saveNumber(x,i,j) {
+  var y = +x;
+	this.data1[i][j] = y ;
+  }
+  
+  validate(){
+	var myJsonString = JSON.stringify(this.data1);
+  console.log(this.data1);
+  //alert("you win !");
+  console.log(myJsonString);
+  this.http.post("http://localhost:4200/sudoku-facile/post", {"moo":"foo","goo":"loo"}).subscribe(res => console.log(res.json()), const headers = new Headers({ 'Content-Type': 'application/x-www-form-urlencoded' }));
+  //return myJsonString ;
+  
+  }
+  
   
 
 }
