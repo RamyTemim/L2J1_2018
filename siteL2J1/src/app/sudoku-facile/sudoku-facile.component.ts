e0a636326a8e416610d05c3c022df8ebbb685ec6
saadou11 <saadallah.mahi@gmail.com>
2018-04-02 23:30:08 +0200

    easy sudoku

--- siteL2J1/src/app/sudoku-facile/sudoku-facile.component.ts
+++ siteL2J1/src/app/sudoku-facile/sudoku-facile.component.ts
@@ -12,44 +12,44 @@ import 'rxjs/add/operator/map';
 export class SudokuFacileComponent implements OnInit {
   private sudokuFacileUrl = 'http://localhost:8080/sudoku/sudokuFacile';
   data1:  number[][];
-
-
+  
   constructor(private http: Http,private http2: HttpClient) {
 
   this.getGrilleFacile();
-
   }
 
   getDataFacile(){
+  
     return this.http.get(this.sudokuFacileUrl)
     .map((res: Response) => res.json())
   }
   
-
   getGrilleFacile(){
+  
 	this.getDataFacile().subscribe(data1 =>{
       this.data1 = data1
     })
   }
   
   ngOnInit() {
-
   }
   
   saveNumber(x,i,j) {
+  
   var y = +x;
 	this.data1[i][j] = y ;
   }
-  
+
   validate(){
-	var myJsonString = JSON.stringify(this.data1);
-  console.log(this.data1);
-  //alert("you win !");
-  console.log(myJsonString);
-  this.http.post("http://localhost:4200/sudoku-facile/post", {moo:"foo",goo:"loo"}).subscribe(res => console.log(res.json()));
-  //return myJsonString ;
   
+	var myJsonString = JSON.stringify(this.data1);
+
+ this.http.post("http://localhost:8080/sudoku/result", {myJsonString}).subscribe(resultat => {
+ if(resultat.json())
+	{alert("you win !");}
+  else
+	{alert("you lose");}
+  
+ });
   }
-
-
 }
