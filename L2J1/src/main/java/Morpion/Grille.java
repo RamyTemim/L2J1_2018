cdca94bf424de54d18291669a895bc01440a7eeb
Mickael Dai <mickael.dai2609@gmail.com>
2018-04-20 02:37:21 +0200

    correction du reset

--- L2J1/src/main/java/Morpion/Grille.java
+++ L2J1/src/main/java/Morpion/Grille.java
@@ -97,6 +97,15 @@ public Joueur getGagnant () {
 	 return winner;
 	}
 
+/////////// reset grille;
+	public void clear() {
+		for (int i = 0; i < grille.length; i++) {
+			if (this.grille[i]== null)
+			this.grille[i] = new CaseGrille(i);
+	        else 
+			this.grille[i].setContenu(null);
+	}
+	}
 
 
 
