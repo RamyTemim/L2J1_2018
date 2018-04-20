ae31a70908bf8d28e4d008d09aa6874500986681
Mickael Dai <mickael.dai2609@gmail.com>
2018-04-20 10:37:27 +0200

    modif du reset

--- L2J1/src/main/java/services/MorpionService.java
+++ L2J1/src/main/java/services/MorpionService.java
@@ -82,7 +82,7 @@ public class MorpionService {
 	    }
 	    
 /////////// reset grille;
-	   // @JsonIgnore
+	   @JsonIgnore
 	public int  clear(int rep) {
 		if (rep == 1) {
 		for (int i = 0; i < 9; i++) {
@@ -92,9 +92,9 @@ public class MorpionService {
 			this.grille.setCaseContenu(i, null);
 			
 		}
-		 //currentJoueur.setId(joueur1.getId());
+		 currentJoueur = joueur1;
 		}
-			return 3;
+			return 4;
 	
 	}
 	    
